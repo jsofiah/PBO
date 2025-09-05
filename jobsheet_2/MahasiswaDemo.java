53db4172b2af76b93fd6ac7bbc8a8da8d5fac72e
jsofiah <j.sofiah18@gmail.com>
2025-09-05 08:07:29 +0700

    Percobaan 1 Modif

--- jobsheet_2/MahasiswaDemo.java
+++ jobsheet_2/MahasiswaDemo.java
@@ -6,6 +6,22 @@ public class MahasiswaDemo {
         m1.alamat = "Nias, Sumatera Utara";
         m1.kelas = "2A";
 
+        Mahasiswa m2 = new Mahasiswa();
+        m2.nim = "244107060065";
+        m2.nama = "Sofiah";
+        m2.alamat = "Malang, Jawa Timur";
+        m2.kelas = "2D";
+
+        Mahasiswa m3 = new Mahasiswa();
+        m3.nim = "023434";
+        m3.nama = "Budi Bubadibako";
+        m3.alamat = "Jiran, Malaysia";
+        m3.kelas = "2B";
+
         m1.displayBiodata();
+        System.out.println();
+        m2.displayBiodata();
+        System.out.println();
+        m3.displayBiodata();
     }
 }
