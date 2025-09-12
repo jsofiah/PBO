66ce3a35fdd4542b84632fdd267af8230543a5eb
jsofiah <j.sofiah18@gmail.com>
2025-09-12 07:56:20 +0700

    Percobaan 1 No 9 & 10

--- jobsheet_3/motorDemo.java
+++ jobsheet_3/motorDemo.java
@@ -6,18 +6,36 @@ public class motorDemo {
         motor1.displayStatus();
 
         motor1.platNomor = "B 0838 XZ";
-        motor1.kecepatan = 50;
+        int kecepatanBaru = 50;
+        if(!motor1.isMesinon && kecepatanBaru > 0){
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            motor1.kecepatan = kecepatanBaru;
+        }
+        // motor1.kecepatan = 50;
         motor1.displayStatus();
 
         motor motor2 = new motor();
         motor2.platNomor = "N 9840 AB";
         motor2.isMesinon = true;
-        motor2.kecepatan = 40;
+        kecepatanBaru = 40;
+        if(!motor2.isMesinon && kecepatanBaru > 0){
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            motor2.kecepatan = kecepatanBaru;
+        }
+        // motor2.kecepatan = 40;
         motor2.displayStatus();
 
         motor motor3 = new motor();
         motor3.platNomor = "D 8343 CV";
-        motor3.kecepatan = 60;
+        kecepatanBaru = 60;
+        if(!motor1.isMesinon && kecepatanBaru > 0){
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            motor1.kecepatan = kecepatanBaru;
+        }
+        // motor3.kecepatan = 60;
         motor3.displayStatus();
     }
 }
