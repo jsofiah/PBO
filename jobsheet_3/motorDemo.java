dbcfd4ac9f7cd5b89ebad3397455679d02c5ccac
jsofiah <j.sofiah18@gmail.com>
2025-09-12 07:45:58 +0700

    Percobaan 1 No 6

--- jobsheet_3/motorDemo.java
+++ jobsheet_3/motorDemo.java
@@ -8,5 +8,16 @@ public class motorDemo {
         motor1.platNomor = "B 0838 XZ";
         motor1.kecepatan = 50;
         motor1.displayStatus();
+
+        motor motor2 = new motor();
+        motor2.platNomor = "N 9840 AB";
+        motor2.isMesinon = true;
+        motor2.kecepatan = 40;
+        motor2.displayStatus();
+
+        motor motor3 = new motor();
+        motor3.platNomor = "D 8343 CV";
+        motor3.kecepatan = 60;
+        motor3.displayStatus();
     }
 }
