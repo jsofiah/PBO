d3d238c5b8f04ea6794d0c70e1aacc4bd6005b62
jsofiah <j.sofiah18@gmail.com>
2025-09-12 08:19:15 +0700

    Percobaan 2 No 6

--- jobsheet_3/motorDemo.java
+++ jobsheet_3/motorDemo.java
@@ -5,37 +5,19 @@ public class motorDemo {
         motor motor1 = new motor();
         motor1.displayStatus();
 
-        motor1.platNomor = "B 0838 XZ";
-        int kecepatanBaru = 50;
-        if(!motor1.isMesinon && kecepatanBaru > 0){
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-        } else {
-            motor1.kecepatan = kecepatanBaru;
-        }
-        // motor1.kecepatan = 50;
+        motor1.setPlatNomor("B 0838 XZ");
+        motor1.setKecepatan(50);
         motor1.displayStatus();
 
         motor motor2 = new motor();
-        motor2.platNomor = "N 9840 AB";
-        motor2.isMesinon = true;
-        kecepatanBaru = 40;
-        if(!motor2.isMesinon && kecepatanBaru > 0){
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-        } else {
-            motor2.kecepatan = kecepatanBaru;
-        }
-        // motor2.kecepatan = 40;
+        motor2.setPlatNomor("N 9840 AB");
+        motor2.setIsMesinOn(true);
+        motor2.setKecepatan(40);
         motor2.displayStatus();
 
         motor motor3 = new motor();
-        motor3.platNomor = "D 8343 CV";
-        kecepatanBaru = 60;
-        if(!motor1.isMesinon && kecepatanBaru > 0){
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-        } else {
-            motor1.kecepatan = kecepatanBaru;
-        }
-        // motor3.kecepatan = 60;
+        motor3.setPlatNomor("D 8343 CV");
+        motor3.setKecepatan(60);
         motor3.displayStatus();
     }
 }
