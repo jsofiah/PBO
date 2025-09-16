413cc82fd3ad96dd224baa4e99eb3fdb4f24e64f
jsofiah <j.sofiah18@gmail.com>
2025-09-16 14:40:13 +0700

    Tugas 1

---
+++ jobsheet_3/tugas/Anggota.java
@@ -0,0 +1,42 @@
+package jobsheet_3.tugas;
+
+public class Anggota {
+    private String noKTP;
+    private String nama;
+    private int limitPinjam;
+    private int jumlahPinjam;
+
+    public Anggota(String noKTP, String nama, int limitPinjam) {
+        this.noKTP = noKTP;
+        this.nama = nama;
+        this.limitPinjam = limitPinjam;
+        this.jumlahPinjam = 0;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public int getLimitPinjaman(){
+        return limitPinjam - jumlahPinjam;
+    }
+
+    public void pinjam(int jumlah){
+        if(jumlah + jumlahPinjam > limitPinjam){
+            System.out.println("Maaf, jumlah pinjaman melebihi limit");
+        } else {
+            jumlahPinjam += jumlah;
+        }
+    }
+
+    public int getJumlahPinjaman(){
+        return jumlahPinjam;
+    }
+
+    public void angsur(int jmlAngsur){
+        jumlahPinjam -= jmlAngsur;
+        if (jumlahPinjam < 0) {
+            jumlahPinjam = 0;
+        }
+    }
+}
