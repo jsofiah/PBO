aeca8352b16f54ba9fb7ad181c520bec6c468b07
jsofiah <j.sofiah18@gmail.com>
2025-10-03 08:57:53 +0700

    Percobaan 4 no 1 & 2

--- jobsheet_6/Dosen.java
+++ jobsheet_6/Dosen.java
@@ -6,5 +6,14 @@ public class Dosen extends Pegawai {
         System.out.println(gaji);
         System.out.println("Objek dari class Dosen dibuat");
     }
+
+    public String getAllInfo(){
+        String info = "";
+        info += "NIP         : " + nip + "\n";
+        info += "Nama        : " + nama + "\n";
+        info += "Gaji        : " + gaji + "\n";
+        info += "NIDN        : " + nidn + "\n";
+        return info;
+    }
     
 }
