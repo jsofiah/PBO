9e2e8e8428293322fd5b3bc9b6a9600017ba7c3e
jsofiah <j.sofiah18@gmail.com>
2025-10-03 09:47:36 +0700

    Percobaan 7 no 2

--- jobsheet_6/Dosen.java
+++ jobsheet_6/Dosen.java
@@ -8,9 +8,9 @@ public class Dosen extends Pegawai {
     }
 
     public Dosen(String nip, String nama, double gaji, String nidn) {
-        this.nip = nip;
-        this.nama = nama;
-        this.gaji = gaji;
+        super.nip = nip;
+        super.nama = nama;
+        super.gaji = gaji;
         this.nidn = nidn;
     }
 
