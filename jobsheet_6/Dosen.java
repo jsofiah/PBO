235d9af255660753637ba7d41eaf89e304703f68
jsofiah <j.sofiah18@gmail.com>
2025-10-03 07:09:03 +0700

    Percobaan 2

--- jobsheet_6/Dosen.java
+++ jobsheet_6/Dosen.java
@@ -1,7 +1,8 @@
 package jobsheet_6;
 
 public class Dosen extends Pegawai {
-
+    public String nidn;
+    
     public Dosen() {
         System.out.println("Objek dari class Dosen dibuat");
     }
