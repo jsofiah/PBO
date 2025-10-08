e83b1c037418a5a2a33a4de018a1a2209b2c5443
jsofiah <j.sofiah18@gmail.com>
2025-10-08 22:21:13 +0700

    Percobaan 7 no 6

--- jobsheet_7/Dosen.java
+++ jobsheet_7/Dosen.java
@@ -1,17 +1,17 @@
 public class Dosen extends Pegawai{
     public String nidn;
 
-    public Dosen() {
-        // System.out.println(gaji);
-        System.out.println("Objek dari class Dosen dibuat");
-    }
+    // public Dosen() {
+    //     // System.out.println(gaji);
+    //     System.out.println("Objek dari class Dosen dibuat");
+    // }
 
     // public Dosen(String nip, String nama, double gaji,String nidn) {
     //     System.out.println("Objek dari class Dosen dibuat dengan constructor berparameter");
     // }
     public Dosen(String nip, String nama, double gaji,String nidn) {
-        this.nidn = nidn;
         super(nip, nama, gaji);
+        this.nidn = nidn;
     }
 
     public String getInfo(){
