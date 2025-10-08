f022920a44039271f995a53390609ed64514d90d
jsofiah <j.sofiah18@gmail.com>
2025-10-08 22:10:41 +0700

    Percobaan 7 no 4

--- jobsheet_7/Pegawai.java
+++ jobsheet_7/Pegawai.java
@@ -3,8 +3,13 @@ public class Pegawai {
     public String nama;
     protected double gaji;
 
-    public Pegawai() {
-        System.out.println("Objek dari class Pegawai dibuat");
+    // public Pegawai() {
+    //     System.out.println("Objek dari class Pegawai dibuat");
+    // }
+    public Pegawai(String nip, String nama, double gaji) {
+        this.nip = nip;
+        this.nama = nama;
+        this.gaji = gaji;
     }
 
     public String getInfo(){
