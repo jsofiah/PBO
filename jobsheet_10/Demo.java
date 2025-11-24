c2c1173695f2de3aa121531adcda20b3fa88aa3e
jsofiah <j.sofiah18@gmail.com>
2025-11-24 12:43:09 +0700

    Object Casting no 5

--- jobsheet_10/Demo.java
+++ jobsheet_10/Demo.java
@@ -2,10 +2,12 @@
 public class Demo {
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
+
+        Pegawai pegawai1 = dosen1;
         
-        System.out.println(dosen1.nip);
-        System.out.println(dosen1.nama);
-        System.out.println(dosen1.nidn);
-        dosen1.mengajar();
+        System.out.println(pegawai1.nip);
+        System.out.println(pegawai1.nama);
+        System.out.println(pegawai1.nidn);
+        pegawai1.mengajar();
     }
 }
