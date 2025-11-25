afa6c23a32b6e193c2953f46d53a96d9ffa536a1
jsofiah <j.sofiah18@gmail.com>
2025-11-25 07:00:54 +0700

    Polymorphic Arguments & Instance of no 3

--- jobsheet_10/Demo.java
+++ jobsheet_10/Demo.java
@@ -1,18 +1,14 @@
-
 public class Demo {
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
-
-        Pegawai pegawai1 = dosen1;
+        TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", "Aida, A.Md.", "Tenaga Administrasi");
         
-        System.out.println(pegawai1.nip);
-        System.out.println(pegawai1.nama);
-        pegawai1.displayInfo();
+        train(dosen1);
+        train(tendik1);
+    }
 
-        Dosen newDosen = (Dosen) pegawai1;
-
-        System.out.println(newDosen.nama);
-        System.out.println(newDosen.nidn);
-        newDosen.mengajar();
+    public static void train(Pegawai pegawai){
+        System.out.println("Memberikan pelatihan untuk pegawai");
+        pegawai.displayInfo();
     }
 }
